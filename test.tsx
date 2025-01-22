412137b66ca8a840d481955a5ae2b40e9189326d
Hemant Kumar Yadav <hemant.yadav@sumologic.com>
2025-01-22 17:28:52 +0530

    add change

---
+++ test.tsx
@@ -0,0 +1,7 @@
+<div className={styles.cardHeader}>
+        <Text variant="textHeavy">{groupTitle}</Text>
+        <Tooltip title={tooltipText}>
+        <Tooltip title={tooltipText} data-sumo-test="app-info-tooltip">
+          <Icon name={IconName.Info} />
+        </Tooltip>
+      </div>
