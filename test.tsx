4ca2e108c1d1ef733c83251ca1656f4e51b58c94
Hemant Kumar Yadav <hemant.yadav@sumologic.com>
2025-01-27 13:58:00 +0530

    add changes in locator

--- test.tsx
+++ test.tsx
@@ -1,7 +1,7 @@
 <div className={styles.cardHeader}>
         <Text variant="textHeavy">{groupTitle}</Text>
         <Tooltip title={tooltipText}>
-        <Tooltip title={tooltipText} data-sumo-test="upgrade-payment-Data Volume-selectors">
+        <Tooltip title={tooltipText} data-sumo-test="upgrade-payment-Data Volumes">
           <Icon name={IconName.Info} />
         </Tooltip>
       </div>
